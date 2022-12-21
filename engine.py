b7bcf8230670fa3d5d60d23d04886c0add32bbf2
yon co <67898615+yonatan-co@users.noreply.github.com>
2022-12-21 13:05:36 +0200

    Update engine.py

--- engine.py
+++ engine.py
@@ -116,77 +116,101 @@ def test_step(model: torch.nn.Module,
   test_acc = test_acc / len(dataloader)
   return test_loss, test_acc
 
-def train(model: torch.nn.Module, 
+import wandb
+
+
+# Import train() function from: 
+# https://github.com/mrdbourke/pytorch-deep-learning/blob/main/going_modular/going_modular/engine.py
+def train(model: torch.nn.Module,
           train_dataloader: torch.utils.data.DataLoader, 
           test_dataloader: torch.utils.data.DataLoader, 
           optimizer: torch.optim.Optimizer,
           loss_fn: torch.nn.Module,
           epochs: int,
-          device: torch.device) -> Dict[str, List]:
-  """Trains and tests a PyTorch model.
+          device: torch.device,
+          use_wandb: bool=True) -> Dict[str, List]:
+    """Trains and tests a PyTorch model.
 
-  Passes a target PyTorch models through train_step() and test_step()
-  functions for a number of epochs, training and testing the model
-  in the same epoch loop.
+    Passes a target PyTorch models through train_step() and test_step()
+    functions for a number of epochs, training and testing the model
+    in the same epoch loop.
 
-  Calculates, prints and stores evaluation metrics throughout.
+    Calculates, prints and stores evaluation metrics throughout.
 
-  Args:
-    model: A PyTorch model to be trained and tested.
-    train_dataloader: A DataLoader instance for the model to be trained on.
-    test_dataloader: A DataLoader instance for the model to be tested on.
-    optimizer: A PyTorch optimizer to help minimize the loss function.
-    loss_fn: A PyTorch loss function to calculate loss on both datasets.
-    epochs: An integer indicating how many epochs to train for.
-    device: A target device to compute on (e.g. "cuda" or "cpu").
+    Args:
+      model: A PyTorch model to be trained and tested.
+      train_dataloader: A DataLoader instance for the model to be trained on.
+      test_dataloader: A DataLoader instance for the model to be tested on.
+      optimizer: A PyTorch optimizer to help minimize the loss function.
+      loss_fn: A PyTorch loss function to calculate loss on both datasets.
+      epochs: An integer indicating how many epochs to train for.
+      use_wandb: To wheter log results to weights & biases or not.
+      device: A target device to compute on (e.g. "cuda" or "cpu").
+      
+    Returns:
+      A dictionary of training and testing loss as well as training and
+      testing accuracy metrics. Each metric has a value in a list for 
+      each epoch.
+      In the form: {train_loss: [...],
+                train_acc: [...],
+                test_loss: [...],
+                test_acc: [...]} 
+      For example if training for epochs=2: 
+              {train_loss: [2.0616, 1.0537],
+                train_acc: [0.3945, 0.3945],
+                test_loss: [1.2641, 1.5706],
+                test_acc: [0.3400, 0.2973]} 
+    """
+    if use_wandb: 
+      wandb.watch(model)
 
-  Returns:
-    A dictionary of training and testing loss as well as training and
-    testing accuracy metrics. Each metric has a value in a list for 
-    each epoch.
-    In the form: {train_loss: [...],
-                  train_acc: [...],
-                  test_loss: [...],
-                  test_acc: [...]} 
-    For example if training for epochs=2: 
-                 {train_loss: [2.0616, 1.0537],
-                  train_acc: [0.3945, 0.3945],
-                  test_loss: [1.2641, 1.5706],
-                  test_acc: [0.3400, 0.2973]} 
-  """
-  # Create empty results dictionary
-  results = {"train_loss": [],
-      "train_acc": [],
-      "test_loss": [],
-      "test_acc": []
-  }
+    # Create empty results dictionary
+    results = {"train_loss": [],
+               "train_acc": [],
+               "test_loss": [],
+               "test_acc": []
+    }
 
-  # Loop through training and testing steps for a number of epochs
-  for epoch in tqdm(range(epochs)):
-      train_loss, train_acc = train_step(model=model,
-                                          dataloader=train_dataloader,
-                                          loss_fn=loss_fn,
-                                          optimizer=optimizer,
-                                          device=device)
-      test_loss, test_acc = test_step(model=model,
-          dataloader=test_dataloader,
-          loss_fn=loss_fn,
-          device=device)
-
-      # Print out what's happening
-      print(
+    # Loop through training and testing steps for a number of epochs
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model=model,
+                                           dataloader=train_dataloader,
+                                           loss_fn=loss_fn,
+                                           optimizer=optimizer,
+                                           device=device)
+        test_loss, test_acc = test_step(model=model,
+                                        dataloader=test_dataloader,
+                                        loss_fn=loss_fn,
+                                        device=device)
+        if use_wandb:
+          wandb.log({
+                 "train_loss": train_loss,
+                 "train_acc": train_acc,
+                 "test_loss": test_loss,
+                 "test_acc": test_acc
+                  },
+                  step=epoch)
+    
+        # Print out what's happening
+        print(
           f"Epoch: {epoch+1} | "
           f"train_loss: {train_loss:.4f} | "
           f"train_acc: {train_acc:.4f} | "
           f"test_loss: {test_loss:.4f} | "
           f"test_acc: {test_acc:.4f}"
-      )
+        )
 
-      # Update results dictionary
-      results["train_loss"].append(train_loss)
-      results["train_acc"].append(train_acc)
-      results["test_loss"].append(test_loss)
-      results["test_acc"].append(test_acc)
+        # Update results dictionary
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
 
-  # Return the filled results at the end of the epochs
-  return results
+        ### New: Experiment tracking ###
+        # Add loss results to SummaryWriter
+        
+    
+    ### End new ###
+
+    # Return the filled results at the end of the epochs
+    return results
