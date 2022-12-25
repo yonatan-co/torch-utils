9b6726495c07a55067247f8f20a251207298d856
yon co <67898615+yonatan-co@users.noreply.github.com>
2022-12-25 11:32:41 +0200

    Update data_setup.py

--- data_setup.py
+++ data_setup.py
@@ -16,30 +16,7 @@ def create_dataloaders(
     batch_size: int, 
     num_workers: int=NUM_WORKERS
 ):
-  """Creates training and testing DataLoaders.
-
-  Takes in a training directory and testing directory path and turns
-  them into PyTorch Datasets and then into PyTorch DataLoaders.
-
-  Args:
-    train_dir: Path to training directory.
-    test_dir: Path to testing directory.
-    transform: torchvision transforms to perform on training and testing data.
-    batch_size: Number of samples per batch in each of the DataLoaders.
-    num_workers: An integer for number of workers per DataLoader.
-
-  Returns:
-    A tuple of (train_dataloader, test_dataloader, class_names).
-    Where class_names is a list of the target classes.
-    Example usage:
-      train_dataloader, test_dataloader, class_names = \
-        = create_dataloaders(train_dir=path/to/train_dir,
-                             test_dir=path/to/test_dir,
-                             transform=some_transform,
-                             batch_size=32,
-                             num_workers=4)
-  """
-  # Use ImageFolder to create dataset(s)
+ 
   train_data = datasets.ImageFolder(train_dir, transform=transform)
   test_data = datasets.ImageFolder(test_dir, transform=transform)
 
